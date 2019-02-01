c3b86f0ce6ffe6f9b8c55db047ce7268c7da3b58
amichaiman <ami.dan.man@gmail.com>
2019-02-01 18:47:38 +0200

    cpp spring 16 part b q 1a

---
+++ Stack.h
@@ -0,0 +1,32 @@
+//
+// Created by amichai on 01/02/19.
+//
+
+#ifndef MARATHON_CPP_2019_STACK_H
+#define MARATHON_CPP_2019_STACK_H
+#include <exception>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+class Stack {
+public:
+    Stack& Stack(const Stack& rhs);
+    Stack& operator=(const Stack& rhs);
+    bool push(T& elem);
+    T& top() const throw(EmptyStackException);
+    void pop() throw(EmptyStackException);
+    int size() const;
+    bool empty() const;
+
+    struct EmptyStackException : public exception {
+        const char* what() const throw {
+            return "Stack is empty";
+        }
+    };
+
+private:
+    vector v;
+};
+#endif //MARATHON_CPP_2019_STACK_H
