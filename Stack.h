210853856363610855c108d2d28b2145b1ec6429
amichaiman <ami.dan.man@gmail.com>
2019-02-06 09:44:09 +0200

    in marathon

--- Stack.h
+++ Stack.h
@@ -1,10 +1,9 @@
 //
-// Created by amichai on 01/02/19.
+// Created by amichai on 06/02/19.
 //
 
-#ifndef MARATHON_CPP_2019_STACK_H
-#define MARATHON_CPP_2019_STACK_H
-#include <exception>
+#ifndef MARATHON_CPP_2019_STACKMARATHON_H
+#define MARATHON_CPP_2019_STACKMARATHON_H
 #include <vector>
 
 using namespace std;
@@ -12,20 +11,20 @@ using namespace std;
 template <typename T>
 class Stack {
 public:
-    explicit Stack();
-    bool push(T& elem);
+    void push(const T &t);
     T& top() const throw(EmptyStackException);
     void pop() throw(EmptyStackException);
     int size() const;
     bool empty() const;
 
-    struct EmptyStackException : public exception {
-        const char* what() const throw {
-            return "Stack is empty";
+    struct EmptyStackException : exception {
+        virtual const char* what() const throw() {
+            return "stack is empty";
         }
     };
-
 private:
-    vector v;
+    vector<T> v;
 };
-#endif //MARATHON_CPP_2019_STACK_H
+
+
+#endif //MARATHON_CPP_2019_STACKMARATHON_H
